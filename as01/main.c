92b796e8325a41c40969f002b4a745f8c1bbc6c4
root <root@elb097258-1724333350.us-east-1.elb.amazonaws.com>
2018-09-10 19:04:29 +0200

    l

---
+++ as01/main.c
@@ -0,0 +1,20 @@
+#include <linux/module.h>
+#include <linux/init.h>
+#include <linux/kernel.h>
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Mapumba Mark NTUMBA <marmap4@gmail.com>");
+MODULE_DESCRIPTION("hello world module");
+
+static int __init entry_point(void)
+{
+	printk(KERN_INFO "Hello world!\n");
+	return 0;
+}
+
+static void __exit exit_point(void)
+{
+	printk(KERN_INFO "Cleaning up module.\n");
+}
+module_init(entry_point);
+module_exit(exit_point)
